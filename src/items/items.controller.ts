9c350ad245dccab6fdc52802ba6c3ddcafc1b952
junioricntra <junior.cintra.developer@gmail.com>
2024-09-06 03:55:03 -0300

    export items to excel

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,7 +6,10 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
 
@@ -22,6 +25,29 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Get('exportItems')
+  async exportItemsExcel(@Res() res: Response) {
+    try {
+      const excelBuffer = await this.itemsService.generateExcelItems();
+
+      res.setHeader(
+        'Content-Type',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      );
+      res.setHeader(
+        'Content-Disposition',
+        'attachment; filename=profissionais.xlsx',
+      );
+
+      res.send(excelBuffer);
+    } catch (error) {
+      console.error('Error generating Excel', error);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+        error: { description: error.message },
+      });
+    }
   }
 
   @Get(':id')
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import * as ExcelJS from 'exceljs';
 
 @Injectable()
 export class ItemsService {
@@ -37,5 +38,34 @@ export class ItemsService {
     const itemDeleted = this.prisma.item.delete({ where: { id: id } });
 
     return itemDeleted;
+  }
+
+  async generateExcelItems(): Promise<Buffer> {
+    // Supondo que o modelo 'professional' exista no seu Prisma Service
+    const profissionais = await this.prisma.item.findMany();
+
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Profissionais');
+
+    worksheet.columns = [
+      { header: 'ID', key: 'id', width: 10 },
+      { header: 'Nome', key: 'nome', width: 30 },
+      { header: 'Descrição', key: 'descricao', width: 30 },
+      { header: 'Imagem', key: 'image', width: 50 },
+    ];
+
+    profissionais.forEach((item) => {
+      worksheet.addRow({
+        id: item.id,
+        nome: item.nome,
+        descricao: item.descricao,
+        image: item.image,
+      });
+    });
+
+    // Converter o ArrayBuffer para Buffer
+    const buffer = Buffer.from(await workbook.xlsx.writeBuffer());
+
+    return buffer;
   }
 }
